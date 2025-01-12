3e378c1f3852c6efcd48f5e8143e5a887b5afccc
Lin Thit Htoo <lth1212001@gmail.com>
2025-01-13 00:03:51 +0630

    -

--- PollyCircuitBreaker/Models/ProductModel.cs
+++ PollyCircuitBreaker/Models/ProductModel.cs
@@ -1,19 +1,18 @@
-﻿namespace PollyCircuitBreaker.Models
-{
-    public class ProductModel
-    {
-        public int id { get; set; }
-        public string title { get; set; }
-        public float price { get; set; }
-        public string description { get; set; }
-        public string category { get; set; }
-        public string image { get; set; }
-        public Rating rating { get; set; }
-    }
+﻿namespace PollyCircuitBreaker.Models;
 
-    public class Rating
-    {
-        public float rate { get; set; }
-        public int count { get; set; }
-    }
+public class ProductModel
+{
+    public int id { get; set; }
+    public string title { get; set; }
+    public float price { get; set; }
+    public string description { get; set; }
+    public string category { get; set; }
+    public string image { get; set; }
+    public Rating rating { get; set; }
+}
+
+public class Rating
+{
+    public float rate { get; set; }
+    public int count { get; set; }
 }
