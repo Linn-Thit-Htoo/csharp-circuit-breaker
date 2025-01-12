b16980f5158d326cd6f2568e0fdaa10ccab8f1a1
Lin Thit Htoo <lth1212001@gmail.com>
2025-01-13 00:03:45 +0630

    -

--- PollyCircuitBreaker/Extensions/DependencyInjectionExtensions.cs
+++ PollyCircuitBreaker/Extensions/DependencyInjectionExtensions.cs
@@ -5,25 +5,31 @@ namespace PollyCircuitBreaker.Extensions;
 
 public static class DependencyInjectionExtensions
 {
-    public static IServiceCollection AddDependencies(this IServiceCollection services, WebApplicationBuilder builder)
+    public static IServiceCollection AddDependencies(
+        this IServiceCollection services,
+        WebApplicationBuilder builder
+    )
     {
-        builder.Services.AddHttpClient("FakeStoreClient", opt =>
-        {
-            opt.BaseAddress = new Uri("https://fakestoreapis.com");
-        });
+        builder.Services.AddHttpClient(
+            "FakeStoreClient",
+            opt =>
+            {
+                opt.BaseAddress = new Uri("https://fakestoreapis.com");
+            }
+        );
 
         var circuitBreakerPolicy = Policy
-        .Handle<HttpRequestException>()
-        .CircuitBreakerAsync(
-            exceptionsAllowedBeforeBreaking: 2,
-            durationOfBreak: TimeSpan.FromMinutes(1),
-            onBreak: (exception, timespan) =>
-            {
-                Console.WriteLine($"Circuit broken due to: {exception.Message}");
-            },
-            onReset: () => Console.WriteLine("Circuit closed."),
-            onHalfOpen: () => Console.WriteLine("Circuit in half-open state.")
-        );
+            .Handle<HttpRequestException>()
+            .CircuitBreakerAsync(
+                exceptionsAllowedBeforeBreaking: 2,
+                durationOfBreak: TimeSpan.FromMinutes(1),
+                onBreak: (exception, timespan) =>
+                {
+                    Console.WriteLine($"Circuit broken due to: {exception.Message}");
+                },
+                onReset: () => Console.WriteLine("Circuit closed."),
+                onHalfOpen: () => Console.WriteLine("Circuit in half-open state.")
+            );
 
         builder.Services.AddSingleton<IAsyncPolicy>(circuitBreakerPolicy);
         builder.Services.AddScoped<IProductService, ProductService>();
