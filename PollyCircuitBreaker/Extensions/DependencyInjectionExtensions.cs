e822231c641857e70f752f3e997300def21fce04
Lin Thit Htoo <lth1212001@gmail.com>
2025-01-13 00:03:41 +0630

    -

--- PollyCircuitBreaker/Extensions/DependencyInjectionExtensions.cs
+++ PollyCircuitBreaker/Extensions/DependencyInjectionExtensions.cs
@@ -1,34 +1,33 @@
 ﻿using Polly;
 using PollyCircuitBreaker.Services;
 
-namespace PollyCircuitBreaker.Extensions
+namespace PollyCircuitBreaker.Extensions;
+
+public static class DependencyInjectionExtensions
 {
-    public static class DependencyInjectionExtensions
+    public static IServiceCollection AddDependencies(this IServiceCollection services, WebApplicationBuilder builder)
     {
-        public static IServiceCollection AddDependencies(this IServiceCollection services, WebApplicationBuilder builder)
+        builder.Services.AddHttpClient("FakeStoreClient", opt =>
         {
-            builder.Services.AddHttpClient("FakeStoreClient", opt =>
+            opt.BaseAddress = new Uri("https://fakestoreapis.com");
+        });
+
+        var circuitBreakerPolicy = Policy
+        .Handle<HttpRequestException>()
+        .CircuitBreakerAsync(
+            exceptionsAllowedBeforeBreaking: 2,
+            durationOfBreak: TimeSpan.FromMinutes(1),
+            onBreak: (exception, timespan) =>
             {
-                opt.BaseAddress = new Uri("https://fakestoreapis.com");
-            });
+                Console.WriteLine($"Circuit broken due to: {exception.Message}");
+            },
+            onReset: () => Console.WriteLine("Circuit closed."),
+            onHalfOpen: () => Console.WriteLine("Circuit in half-open state.")
+        );
 
-            var circuitBreakerPolicy = Policy
-            .Handle<HttpRequestException>()
-            .CircuitBreakerAsync(
-                exceptionsAllowedBeforeBreaking: 2,
-                durationOfBreak: TimeSpan.FromMinutes(1),
-                onBreak: (exception, timespan) =>
-                {
-                    Console.WriteLine($"Circuit broken due to: {exception.Message}");
-                },
-                onReset: () => Console.WriteLine("Circuit closed."),
-                onHalfOpen: () => Console.WriteLine("Circuit in half-open state.")
-            );
+        builder.Services.AddSingleton<IAsyncPolicy>(circuitBreakerPolicy);
+        builder.Services.AddScoped<IProductService, ProductService>();
 
-            builder.Services.AddSingleton<IAsyncPolicy>(circuitBreakerPolicy);
-            builder.Services.AddScoped<IProductService, ProductService>();
-
-            return services;
-        }
+        return services;
     }
 }
